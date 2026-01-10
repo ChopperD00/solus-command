cbbf8b8aca52395161f1999fa9ac32c7f5c0b7f1
Phil Nacionales` <105024295+ChopperD00@users.noreply.github.com>
2026-01-09 16:57:21 -0800

    Add MessageList component

---
+++ src/components/MessageList.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Message as MessageType } from '@/types';
+import { Message } from './Message';
+import { useChatStore } from '@/lib/store';
+import { StreamingIndicator } from './StreamingIndicator';
+
+interface MessageListProps {
+  messages: MessageType[];
+}
+
+export function MessageList({ messages }: MessageListProps) {
+  const { streamState, settings } = useChatStore();
+
+  return (
+    <div className="divide-y divide-white/[0.02]">
+      {messages.map((message, index) => (
+        <Message 
+          key={message.id} 
+          message={message}
+          showIntentDebug={settings.showDebug}
+          isLast={index === messages.length - 1}
+        />
+      ))}
+      
+      {/* Show streaming indicator */}
+      {streamState.isStreaming && (
+        <StreamingIndicator />
+      )}
+    </div>
+  );
+}
